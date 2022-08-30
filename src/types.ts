5dce341ea09d7eefb736d1dc2eb3e158171edfbe
Wesleyzxc <wesleykok-@hotmail.com>
2022-08-30 19:35:39 +1000

    clean: improve types

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,23 @@
-export type AbilityName = 'Mobility' | 'Technique' | 'Survivability' | 'Power' | 'Energy'
+export const AbilityNames = ['Mobility', 'Technique', 'Survivability', 'Power', 'Energy'] as const;
+export type AbilityName = typeof AbilityNames[number];
 
 export interface CharacterAbility {
-  abilityName: AbilityName
-  abilityScore: number
+  abilityName: AbilityName;
+  abilityScore: number;
 }
 
 export interface CharacterTag {
-  slot: number
-  tag_name: string
+  slot: number;
+  tag_name: string;
 }
 
 export interface Character {
-  id: number
-  name: string
-  quote: string
-  image: string
-  thumbnail: string
-  universe: string
-  abilities: CharacterAbility[]
-  tags: CharacterTag[]
+  id: number;
+  name: string;
+  quote?: string;
+  image: string;
+  thumbnail?: string;
+  universe: string;
+  abilities: CharacterAbility[];
+  tags?: CharacterTag[];
 }
