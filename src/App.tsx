03ff0a0544aa2062e5a94422fb27314f6697aec9
Wesleyzxc <wesleykok-@hotmail.com>
2022-08-30 19:35:52 +1000

    feat: add team display to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { characterData } from './data/characters';
 import Header from './components/Header/Header';
+import TeamDisplay from './components/Squad/Squad';
 
 function App() {
   const data = characterData;
   return (
     <>
       <Header />
+      <TeamDisplay />
     </>
   );
 }
